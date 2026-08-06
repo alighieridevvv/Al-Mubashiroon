6dd148869db42199eaa8a0be9ae5d16128b07666
alighieridevvv <khaleelali2007@outlook.com>
2026-08-05 21:09:01 -0400

    No Black Screen

--- android/app/src/main/java/com/almubashshireen/app/MainActivity.java
+++ android/app/src/main/java/com/almubashshireen/app/MainActivity.java
@@ -1,5 +1,16 @@
 package com.almubashshireen.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
+import com.almubashshireen.app.BuildConfig;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+}
